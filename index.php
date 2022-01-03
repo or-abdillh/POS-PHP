bc5780d1d8ec5ee4b2e3a33b14deb4d77c176105
or-abdillh <hans.abdillh05@gmail.com>
2022-01-03 16:03:17 +0700

    Create modal

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
             <div class="card-body">
                 <h5 class="card-title">Create new transaction from order</h5>
                 <p class="card-text">You can create new transaction here</p>
-                <a href="#" class="btn btn-outline-secondary">
+                <a href="#" data-bs-toggle="modal" data-bs-target="#createTransaction" class="btn btn-outline-secondary">
                     <i class="fas fa-arrow-right"></i>
                     Next
                 </a>
@@ -98,6 +98,60 @@
              - made with <i class="fas fa-heart text-danger"></i> and a cup of <i class="fas fa-mug-hot"></i></p>
     </footer>
 
+    <!-- Modal for create new transaction -->
+    <div class="modal fade" id="createTransaction" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Create new transaction</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <!-- Form -->
+                <form action="post">
+                    <!-- Id Transaction -->
+                    <div class="mb-3">
+                        <label for="idTransaction" class="form-label">ID Transaction</label>
+                        <input type="text" placeholder="ex: TRX0001" name="idTransaction" class="form-control" id="idTransaction" aria-describedby="idTransactionHelp">
+                        <div id="idTransactionHelp" class="form-text">transaction ID is used to mark each transaction</div>
+                    </div>
+
+                    <!-- Name of item -->
+                    <div class="mb-3">
+                        <label for="nameOfItem" class="form-label">Name of item</label>
+                        <input type="text" placeholder="ex: Sunsilk For Hiijab 300ml" name="nameOfItem" class="form-control" id="nameOfItem">
+                    </div>
+
+                    <!-- amount of item -->
+                    <label for="amountOfItem" class="form-label">Amounts of item</label>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text">Pcs</span>
+                        <input type="number" placeholder="ex: 2" name="amountOfItem" class="form-control" id="amountOfItem">
+                    </div>
+
+                    <!-- cash from customer -->
+                    <label for="cash" class="form-label">Cash</label>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text">Rp.</span>
+                        <input type="text" placeholder="ex: 45.000,00" name="cash" class="form-control" id="cash">
+                    </div>
+
+                    <!-- money changes customer -->
+                    <label for="moneyChanges" class="form-label">Cash</label>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text">Rp.</span>
+                        <input type="text" name="moneyChanges" class="form-control" id="moneyChanges" readonly>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+            </div>
+        </div>
+    </div>
+    
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
