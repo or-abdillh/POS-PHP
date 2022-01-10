a785786f80fcd02855182da20457f9e39eb8897a
Oka R. Abdillah <hans.abdillh05@gmail.com>
2022-01-11 00:28:27 +0800

    Import DB

--- DB.sql
+++ DB.sql
@@ -1,15 +1,137 @@
-DROP TABLE IF EXISTS accounts;
-DROP TABLE IF EXISTS roles;
+-- MariaDB dump 10.19  Distrib 10.6.4-MariaDB, for Android (aarch64)
+--
+-- Host: localhost    Database: DB_Pos
+-- ------------------------------------------------------
+-- Server version	5.6.38
 
-CREATE TABLE accounts (
-username VARCHAR(15) NOT NULL PRIMARY KEY,
-id_role INT(15) NOT NULL,
-password FLOAT(15) NOT NULL,
-fullname VARCHAR(50) NOT NULL);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE roles (
-id_role INT(15) AUTO_INCREMENT NOT NULL PRIMARY KEY,
-role VARCHAR(15) NOT NULL);
+--
+-- Table structure for table `accounts`
+--
 
-ALTER TABLE transactions ADD CONSTRAINT transactions_item_id_items_item_id FOREIGN KEY (item_id) REFERENCES items(item_id) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE accounts ADD CONSTRAINT accounts_id_role_roles_id_role FOREIGN KEY (id_role) REFERENCES roles(id_role) ON DELETE CASCADE ON UPDATE CASCADE;
+DROP TABLE IF EXISTS `accounts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `accounts` (
+  `username` varchar(15) NOT NULL,
+  `id_role` int(15) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  `fullname` varchar(50) NOT NULL,
+  PRIMARY KEY (`username`),
+  KEY `accounts_id_role_roles_id_role` (`id_role`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `accounts`
+--
+
+LOCK TABLES `accounts` WRITE;
+/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
+INSERT INTO `accounts` VALUES ('owner',1,'owner123','Sandhika Galih'),('staff',2,'staff123','Rizqa Aulia');
+/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `items`
+--
+
+DROP TABLE IF EXISTS `items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `items` (
+  `item_id` int(15) NOT NULL AUTO_INCREMENT,
+  `item_id_txt` varchar(15) NOT NULL,
+  `item_name` varchar(50) NOT NULL,
+  `item_price` varchar(30) NOT NULL,
+  `create_at` varchar(50) NOT NULL,
+  `last_modified` varchar(50) NOT NULL,
+  PRIMARY KEY (`item_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `items`
+--
+
+LOCK TABLES `items` WRITE;
+/*!40000 ALTER TABLE `items` DISABLE KEYS */;
+INSERT INTO `items` VALUES (1,'ITM001','Sunsilk Hijab 300ml','25.000','01/09/2022 11:03:14 pm',''),(2,'ITM002','Pepsodent 450ml','27.000','01/10/2022 11:39:06 pm','');
+/*!40000 ALTER TABLE `items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `roles` (
+  `id_role` int(15) NOT NULL AUTO_INCREMENT,
+  `role` varchar(15) NOT NULL,
+  PRIMARY KEY (`id_role`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'Owner'),(2,'Staff');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transactions`
+--
+
+DROP TABLE IF EXISTS `transactions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transactions` (
+  `transaction_id` int(10) NOT NULL AUTO_INCREMENT,
+  `item_id` int(15) NOT NULL,
+  `transaction_id_txt` varchar(15) NOT NULL,
+  `transaction_create_at` varchar(50) NOT NULL,
+  `transaction_amount` varchar(30) NOT NULL,
+  `transaction_total` varchar(50) NOT NULL,
+  `transaction_cash` varchar(50) NOT NULL,
+  `transaction_money_changes` varchar(50) NOT NULL,
+  PRIMARY KEY (`transaction_id`),
+  KEY `transactions_item_id_items_item_id` (`item_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transactions`
+--
+
+LOCK TABLES `transactions` WRITE;
+/*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
+INSERT INTO `transactions` VALUES (2,1,'TRX001','01/10/2022 11:30:59 pm','2','50.000','100.000','50.000'),(4,2,'TRX003','01/10/2022 11:39:41 pm','1','27.000','30.000','3.000');
+/*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-01-11  0:27:22
